5d879d84c0ff830a30eeee274ab0e9f860979b6d
altantheprodigy <altanpoke9@gmail.com>
2025-02-05 13:55:17 +0700

    update readme example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,43 +1,4 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/tools/pub/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/to/develop-packages).
--->
-
-A package to provide distance between items in a Column, Row, ListView
-
-## Features
-
-By using this dependency you can provide spacing between items.
-
-instead of using SizedBox() individually.
-
-## Getting started
-
-import this 
-```dart
-    import 'package:with_space_between/with_space_between.dart';
-```
-
-## Usage
-
-You can use package by calling 
-
-.withSpaceBetween(width: space5)
-
-in Column, Row or ListView Angle Brackets
-
-## Example Code
-
-```dart
-    import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:with_space_between/with_space_between.dart';
 
 void main() {
@@ -163,8 +124,3 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-
-```
-
-## Additional information
-
